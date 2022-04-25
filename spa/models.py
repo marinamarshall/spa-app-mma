677d4d60e573322a9d790ffb06db0ecba99fcec7
marinamarshall <marinamarshall2@gmail.com>
2022-04-25 06:16:37 +0000

    Add models to models.py - to be continued

--- spa/models.py
+++ spa/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.modelas import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+CATEGORIES = ( 'Facials', 'Hands and Feet', 'Massage')
+
+
+# Treatment Model
+class Treatment(models.Model):
+    """ TreatmentModel """
+    image = CloudinaryField('image', default='placeholder')
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    duration = models.DurationField()
+
+
+# Client Model
+class Client(models.Model):
+    """ ClientModel """
+    first_name = models.CharField(max_length=100, unique=True)
+    last_name = models.CharField(max_length=100, unique=True)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+    date_of_treatment = models.DateField()
+    time_of_treatment = models.TimeField()
+    length_of_treatment = models.DurationField()
+
+
+# Booking Model
+class Booking(models.Model):
+    """ BookingModel """
+    client = 
+
+# Category Model
+class Category(models.Model):
+    """ CategoryModel """
+    categories = models.(
