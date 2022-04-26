bb1b41615b87d052e6df3260412800710e4fc109
marinamarshall <marinamarshall2@gmail.com>
2022-04-26 20:17:24 +0000

    Add DB Models

--- spa/models.py
+++ spa/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.modelas import User
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-CATEGORIES = ( 'Facials', 'Hands and Feet', 'Massage')
+CATEGORIES = ((0, 'Facial'), (1, 'Hands and Feet'), (2, 'Massage'))
+STATUS = ((0, "Pending"), (1, "Booked"))
 
 
 # Treatment Model
@@ -11,9 +12,12 @@ class Treatment(models.Model):
     image = CloudinaryField('image', default='placeholder')
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.IntegerField(choices=CATEGORIES)
     description = models.TextField()
     duration = models.DurationField()
+
+    def __str__(self):
+        return self.title
 
 
 # Client Model
@@ -22,19 +26,22 @@ class Client(models.Model):
     first_name = models.CharField(max_length=100, unique=True)
     last_name = models.CharField(max_length=100, unique=True)
     phone = models.CharField(max_length=10)
-    email = models.EmailField(unique=True)
-    treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE)
-    date_of_treatment = models.DateField()
-    time_of_treatment = models.TimeField()
-    length_of_treatment = models.DurationField()
+    email = models.EmailField()
+    # treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 # Booking Model
 class Booking(models.Model):
     """ BookingModel """
-    client = 
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+    date_of_treatment = models.DateField()
+    time_of_treatment = models.TimeField()
+    # length_of_treatment = models.DurationField()
+    status = models.IntegerField(choices=STATUS, default=0)
 
-# Category Model
-class Category(models.Model):
-    """ CategoryModel """
-    categories = models.(
+    def __str__(self):
+        return f"{self.client} {self.treatment}"
